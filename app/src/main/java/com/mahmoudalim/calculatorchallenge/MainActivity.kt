490933f974e7bffe3eca15aeaf8b5b748b087d0f
Mahmoud-alim <mahmoudhusseinalim@gmail.com>
2021-02-19 17:46:19 +0200

    migrate to ViewBinding

--- app/src/main/java/com/mahmoudalim/calculatorchallenge/MainActivity.kt
+++ app/src/main/java/com/mahmoudalim/calculatorchallenge/MainActivity.kt
@@ -3,11 +3,15 @@ package com.mahmoudalim.calculatorchallenge
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.mahmoudalim.calculatorchallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     fun multiply(view: View) {}
