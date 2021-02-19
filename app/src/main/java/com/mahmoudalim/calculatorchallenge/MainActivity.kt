30e1a663461fbbd898c1ffcd0efc18dfc2c78bdd
Mahmoud-alim <mahmoudhusseinalim@gmail.com>
2021-02-19 17:41:34 +0200

    Layout structure

--- app/src/main/java/com/mahmoudalim/calculatorchallenge/MainActivity.kt
+++ app/src/main/java/com/mahmoudalim/calculatorchallenge/MainActivity.kt
@@ -2,10 +2,19 @@ package com.mahmoudalim.calculatorchallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun multiply(view: View) {}
+    fun divide(view: View) {}
+    fun minus(view: View) {}
+    fun plus(view: View) {}
+    fun undo(view: View) {}
+    fun redo(view: View) {}
+    fun equal(view: View) {}
 }
